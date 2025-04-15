b45952bd50b247eafbac427bc0a0cedc42b25aa0
Mitchell Grams <mitchell.grams@apollomachine.com>
2025-04-14 20:39:13 -0600

    test with modbus driver queue

---
+++ ioports.c
@@ -0,0 +1,322 @@
+/*
+  ioports.c - connect to picoHAL (RP2040) ioexpander via modbus RTU
+
+  Part of grblHAL
+
+  Copyright (c) 2023 Expatria Technologies Inc.
+  Copyright (c) 2025 Terje Io
+  Copyright (c) 2025 Mitchell Grams
+
+  grblHAL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  grblHAL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ioports.h"
+
+#if PICOHAL_IO_ENABLE == 1
+
+static bool analog_out (uint8_t port, float value)
+{
+    if(port < analog.out.n_ports) {
+
+        uint16_t *val = (uint16_t *)aux_aout[port].aux.port; //get current value
+
+        *val = (uint16_t)value;
+
+        modbus_message_t data = {
+            .context = NULL,
+            .crc_check = false,
+            .adu[0] = PICOHAL_ADDRESS,
+            .adu[1] = ModBus_WriteRegister,
+            .adu[2] = (uint8_t)(aux_aout[port].addr >> 8),
+            .adu[3] = (uint8_t)(aux_aout[port].addr & 0xFF),
+            .adu[4] = (uint8_t)(*val >> 8),
+            .adu[5] = (uint8_t)*val,
+            .tx_length = 8,
+            .rx_length = 8
+        };
+
+        picohal_send_message_now(data);
+    }
+
+    return true;
+}
+
+static void digital_out (uint8_t port, bool on)
+{
+    if(port < digital.out.n_ports) {
+
+        uint16_t *val = (uint16_t *)aux_dout[port].aux.port; //get current value
+
+        if(on)
+            *val |= (1 << aux_dout[port].aux.pin);
+        else
+            *val &= ~(1 << aux_dout[port].aux.pin);
+
+        modbus_message_t data = {
+            .context = NULL,
+            .crc_check = false,
+            .adu[0] = PICOHAL_ADDRESS,
+            .adu[1] = ModBus_WriteRegister,
+            .adu[2] = (uint8_t)(aux_dout[port].addr >> 8),
+            .adu[3] = (uint8_t)(aux_dout[port].addr & 0xFF),
+            .adu[4] = 0,
+            .adu[5] = (uint8_t)*val,
+            .tx_length = 8,
+            .rx_length = 8
+        };
+
+        picohal_send_message_now(data);
+    }
+}
+
+static float analog_out_state (xbar_t *output)
+{
+    float value = -1.0f;
+
+    if(output->id < analog.out.n_ports)
+        value = (float)*(uint16_t *)output->port;
+
+    return value;
+}
+
+static float digital_out_state (xbar_t *output)
+{
+    float value = -1.0f;
+
+    if(output->id < digital.out.n_ports)
+        value = (float)(!!(*(uint16_t *)output->port & (1 << output->pin)));
+
+    return value;
+}
+
+static xbar_t *a_get_pin_info (io_port_direction_t dir, uint8_t port)
+{
+    static xbar_t pin;
+
+    xbar_t *info = NULL;
+
+    if(dir == Port_Input && port < analog.in.n_ports) {
+        info = &pin;
+    }
+
+    if(dir == Port_Output && port < analog.out.n_ports) {
+        memcpy(&pin, &aux_aout[port].aux, sizeof(xbar_t));
+        pin.get_value = analog_out_state;
+        info = &pin;
+    }
+
+    return info;
+}
+
+static xbar_t *d_get_pin_info (io_port_direction_t dir, uint8_t port)
+{
+    static xbar_t pin;
+
+    xbar_t *info = NULL;
+
+    if(dir == Port_Input && port < digital.in.n_ports) {
+//...
+        info = &pin;
+    }
+
+    if(dir == Port_Output && port < digital.out.n_ports) {
+        memcpy(&pin, &aux_dout[port].aux, sizeof(xbar_t));
+        pin.get_value = digital_out_state;
+//        pin.set_value = digital_out_state;
+        info = &pin;
+    }
+
+    return info;
+}
+
+static void a_set_pin_description (io_port_direction_t dir, uint8_t port, const char *description)
+{
+//    if(dir == Port_Input && port < analog.in.n_ports)
+//        aux_ain[port].description = description;
+
+    if(dir == Port_Output && port < analog.out.n_ports)
+        aux_aout[port].aux.description = description;
+}
+
+static void d_set_pin_description (io_port_direction_t dir, uint8_t port, const char *description)
+{
+//    if(dir == Port_Input && port < digital.in.n_ports)
+//        aux_din[port].description = description;
+
+    if(dir == Port_Output && port < digital.out.n_ports)
+        aux_dout[port].aux.description = description;
+}
+
+static enumerate_pins_ptr on_enumerate_pins;
+
+static void onEnumeratePins (bool low_level, pin_info_ptr pin_info, void *data)
+{
+    static xbar_t pin = {};
+
+    on_enumerate_pins(low_level, pin_info, data);
+
+    uint_fast8_t idx;
+
+    for(idx = 0; idx < sizeof(aux_dout) / sizeof(picohal_aux_t); idx ++) {
+
+        memcpy(&pin, &aux_dout[idx].aux, sizeof(xbar_t));
+
+        if(!low_level)
+            pin.port = "PicoHAL:";
+
+        pin_info(&pin, data);
+    };
+
+    for(idx = 0; idx < sizeof(aux_aout) / sizeof(picohal_aux_t); idx ++) {
+
+        memcpy(&pin, &aux_aout[idx].aux, sizeof(xbar_t));
+
+        if(!low_level)
+            pin.port = "PicoHAL:";
+
+        pin_info(&pin, data);
+    };
+}
+
+static void get_aux_max (xbar_t *pin, void *data)
+{
+    if(pin->group == PinGroup_AuxOutput)
+        aux_dout_base = max(aux_dout_base, pin->function + 1);
+    else if(pin->group == PinGroup_AuxOutputAnalog)
+        aux_aout_base = max(aux_aout_base, pin->function + 1);
+}
+
+static void picohal_send_message_now (modbus_message_t data){
+
+    task_delete(picohal_send_keepalive, NULL);
+    modbus_send(&data, &callbacks, true);
+
+    task_add_delayed(picohal_send_keepalive, NULL, PICOHAL_KEEPALIVE_INTERVAL);
+}
+
+static void picohal_send_keepalive (void *data){
+
+    modbus_send(&keepalive_msg, &callbacks, false); // should this be non-blocking
+
+    task_add_delayed(picohal_send_keepalive, NULL, PICOHAL_KEEPALIVE_INTERVAL);
+}
+
+static void picohal_rx_packet (modbus_message_t *msg)
+{
+    
+}
+
+static void raise_alarm (void *data)
+{
+    system_raise_alarm(Alarm_AbortCycle);
+    report_message("PicoHAL communication error.", Message_Warning);
+}
+
+static void picohal_rx_exception (uint8_t code, void *context)
+{
+    if(sys.cold_start){ // is this necessary? Copied from vfd
+        protocol_enqueue_foreground_task(raise_alarm, NULL);
+    }
+    else{
+        system_raise_alarm(Alarm_AbortCycle);
+        report_message("PicoHAL communication error.", Message_Warning);
+    }
+}
+
+static void onReportOptions (bool newopt)
+{
+    on_report_options(newopt);
+
+    if(!newopt)
+        report_plugin("PicoHAL IOExpansion", "0.01");
+}
+
+static void OnReset (void)
+{
+    driver_reset();
+}
+
+void picohal_io_init (void) {
+
+    uint_fast8_t idx;
+
+    hal.enumerate_pins(false, get_aux_max, NULL);
+
+    digital.out.n_ports = sizeof(aux_dout) / sizeof(picohal_aux_t);
+
+    for(idx = 0; idx < digital.out.n_ports; idx ++) {
+        aux_dout[idx].addr = PICOHAL_ADDR_DOUT;
+        aux_dout[idx].aux.id = idx;
+        aux_dout[idx].pin = idx;
+        aux_dout[idx].port = &picohal_d_out;
+        aux_dout[idx].aux.function = aux_dout_base + idx;
+        aux_aout[idx].group = PinGroup_AuxOutput;
+        aux_dout[idx].cap.output = On;
+        aux_dout[idx].cap.external = On;
+        aux_dout[idx].cap.async = On;
+        aux_dout[idx].cap.claimable = On;
+        aux_dout[idx].mode.output = On;
+        aux_dout[idx].mode.analog = On;
+}
+
+    io_digital_t dports = {
+        .ports = &digital,
+        .digital_out = digital_out,
+        .get_pin_info = d_get_pin_info,
+        .set_pin_description = d_set_pin_description,
+    };
+
+    ioports_add_digital(&dports);
+
+    analog.out.n_ports = sizeof(aux_aout) / sizeof(picohal_aux_t);
+
+    for(idx = 0; idx < analog.out.n_ports; idx ++) {
+        aux_dout[idx].addr = PICOHAL_ADDR_AOUT + idx; //NEED TO DOUBLE CHECK IF THIS IS RIGHT
+        aux_aout[idx].aux.id = idx; 
+        aux_aout[idx].pin = idx;
+        aux_aout[idx].port = &picohal_a_out[idx]; //NEED TO DOUBLE CHECK IF THIS IS RIGHT
+        aux_aout[idx].aux.function = aux_aout_base + idx;
+        aux_aout[idx].group = PinGroup_AuxOutputAnalog;
+        aux_aout[idx].cap.output = On;
+        aux_aout[idx].cap.analog = On;
+        aux_aout[idx].cap.resolution = Resolution_16bit;
+        aux_aout[idx].cap.external = On;
+        aux_aout[idx].cap.async = On;
+        aux_aout[idx].cap.claimable = On;
+        aux_aout[idx].mode.output = On;
+        aux_aout[idx].mode.analog = On;
+    }
+
+    io_analog_t aports = {
+        .ports = &analog,
+        .analog_out = analog_out,
+        .get_pin_info = a_get_pin_info,
+        .set_pin_description = a_set_pin_description,
+    };
+
+    ioports_add_analog(&aports);
+
+    on_enumerate_pins = hal.enumerate_pins;
+    hal.enumerate_pins = onEnumeratePins;
+
+    on_report_options = grbl.on_report_options;
+    grbl.on_report_options = onReportOptions;
+
+    driver_reset = hal.driver_reset;
+    hal.driver_reset = OnReset;
+
+    task_add_delayed(picohal_send_keepalive, NULL, PICO_KEEPALIVE_INTERVAL);
+}
+
+#endif // PICOHAL_IO_ENABLE
---
+++ ioports.h
@@ -0,0 +1,94 @@
+/*
+  ioports.h - connect to picoHAL (RP2040) ioexpander via modbus RTU
+
+  Part of grblHAL
+
+  Copyright (c) 2023 Expatria Technologies Inc.
+  Copyright (c) 2025 Terje Io
+  Copyright (c) 2025 Mitchell Grams
+
+  grblHAL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  grblHAL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "grbl/hal.h"
+#include "grbl/modbus.h"
+#include "grbl/protocol.h"
+#include "grbl/state_machine.h"
+#include "grbl/task.h"
+
+#ifndef PICOHAL_ADDRESS
+#define PICOHAL_ADDRESS               10
+#endif
+
+#define PICOHAL_RETRIES               5
+#define PICOHAL_RETRY_DELAY           250
+#define PICOHAL_KEEPALIVE_INTERVAL    1000
+
+#ifndef PICOHAL_ADDR_KEEPALIVE
+#define PICOHAL_ADDR_KEEPALIVE   0x0120
+#endif
+
+#ifndef PICOHAL_ADDR_DOUT
+#define PICOHAL_ADDR_DOUT        0x0110
+#endif
+
+#ifndef PICOHAL_ADDR_AOUT
+#define PICOHAL_ADDR_AOUT        0x0121
+#endif
+
+static enumerate_pins_ptr on_enumerate_pins;
+static on_report_options_ptr on_report_options;
+static driver_reset_ptr driver_reset;
+
+static void picohal_rx_packet (modbus_message_t *msg);
+static void picohal_rx_exception (uint8_t code, void *context);
+
+static const modbus_callbacks_t callbacks = {
+    .retries = PICOHAL_RETRIES,
+    .retry_delay = PICOHAL_RETRY_DELAY,    
+    .on_rx_packet = picohal_rx_packet,
+    .on_rx_exception = picohal_rx_exception
+};
+
+typedef struct {
+    uint16_t index;
+    modbus_message_t picohal_packet;
+} QueueItem;
+
+modbus_message_t keepalive_msg = {
+    .context = NULL,
+    .crc_check = false,
+    .adu[0] = PICOHAL_ADDRESS,
+    .adu[1] = ModBus_WriteRegister,
+    .adu[2] = (uint8_t)(PICOHAL_ADDR_KEEPALIVE >> 8),
+    .adu[3] = (uint8_t)(PICOHAL_ADDR_KEEPALIVE & 0xFF),
+    .adu[4] = 0,
+    .adu[5] = 0x01,
+    .tx_length = 8,
+    .rx_length = 8
+};
+
+typedef struct {
+    uint16_t addr;
+    xbar_t aux;
+} picohal_aux_t;
+
+static uint16_t picohal_d_out[1]; // 16 BIT NUMBER IS GOOD FOR UP TO 16 DIGITAL OUTPUTS
+static uint16_t picohal_a_out[2]; // NEEDS TO BE EQUAL TO NUMBER OF ANALOG OUTPUTS?
+static pin_function_t aux_dout_base = Output_Aux0, aux_aout_base = Output_Analog_Aux0;
+static io_ports_data_t analog;
+static io_ports_data_t digital;
+
+static picohal_aux_t aux_dout[8] = {};
+static picohal_aux_t aux_aout[2] = {};
